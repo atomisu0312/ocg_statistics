22f7c7a2678b71b804d2d825298a7d8271cbb7fb
atomisu0312 <koma4024@gmail.com>
2025-08-17 16:42:05 +0900

    【カードマスタ】テーブル定義FIX #37 pushもれ

--- database/migrate/db/migrations/000001_init_ddl.up.sql
+++ database/migrate/db/migrations/000001_init_ddl.up.sql
@@ -140,8 +140,8 @@ CREATE TABLE link_monsters (
 CREATE TABLE pendulum_monsters (
     card_id BIGINT PRIMARY KEY,
     scale INT,
-    text_ja TEXT,
-    text_en TEXT,
+    pendulum_text_ja TEXT,
+    pendulum_text_en TEXT,
     FOREIGN KEY (card_id) REFERENCES monsters(card_id),
     dataowner VARCHAR(255) DEFAULT 'system',
     regist_date TIMESTAMP DEFAULT current_timestamp,
@@ -152,9 +152,9 @@ CREATE TABLE pendulum_monsters (
 
 CREATE TABLE spells (
     card_id BIGINT PRIMARY KEY,
-    id INT,
+    spell_type_id INT,
     FOREIGN KEY (card_id) REFERENCES cards(id),
-    FOREIGN KEY (id) REFERENCES spell_types(id),
+    FOREIGN KEY (spell_type_id) REFERENCES spell_types(id),
     dataowner VARCHAR(255) DEFAULT 'system',
     regist_date TIMESTAMP DEFAULT current_timestamp,
     enable_start_date TIMESTAMP DEFAULT '1970-01-01 00:00:00',
@@ -164,9 +164,9 @@ CREATE TABLE spells (
 
 CREATE TABLE traps (
     card_id BIGINT PRIMARY KEY,
-    id INT,
+    trap_type_id INT,
     FOREIGN KEY (card_id) REFERENCES cards(id),
-    FOREIGN KEY (id) REFERENCES trap_types(id),
+    FOREIGN KEY (trap_type_id) REFERENCES trap_types(id),
     dataowner VARCHAR(255) DEFAULT 'system',
     regist_date TIMESTAMP DEFAULT current_timestamp,
     enable_start_date TIMESTAMP DEFAULT '1970-01-01 00:00:00',
