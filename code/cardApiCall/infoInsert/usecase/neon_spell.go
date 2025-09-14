0bd7da38d99c60d888d26c2fc53e25ad43cbccca
atomisu0312 <koma4024@gmail.com>
2025-09-15 00:28:22 +0900

    【カードマスタ】【Lambda】【データINSERT用】【InsertDB】Magic用のインサート準備 #40
    ユースケースの作成

---
+++ code/cardApiCall/infoInsert/usecase/neon_spell.go
@@ -0,0 +1,54 @@
+package usecase
+
+import (
+	"context"
+	"fmt"
+
+	"atomisu.com/ocg-statics/infoInsert/dto/carddto"
+	"atomisu.com/ocg-statics/infoInsert/repository"
+	"atomisu.com/ocg-statics/infoInsert/sqlc_gen"
+	"atomisu.com/ocg-statics/infoInsert/transaction"
+)
+
+func (n *neonUseCaseImpl) InsertSpellCardInfo(ctx context.Context, cardInfo carddto.StandardCard) (int64, error) {
+
+	tr := transaction.NewTx(n.dbConn.DB)
+
+	result := int64(0)
+
+	err := tr.ExecTx(ctx, func(q *sqlc_gen.Queries) error {
+		// リポジトリの準備
+		cardRepo := repository.NewCardRepository(q)
+		spellRepo := repository.NewSpellRepository(q)
+		spellTypeRepo := repository.NewSpellTypeRepository(q)
+
+		// カードの挿入
+		card, err := cardRepo.InsertCard(ctx, cardInfo.ToInsertCardParamsExceptMonster())
+		if err != nil {
+			return fmt.Errorf("error create card %w", err)
+		}
+
+		// 魔法種別の取得
+		spellType, err := spellTypeRepo.GetSpellTypeByNameEn(ctx, cardInfo.Race)
+		if err != nil {
+			return fmt.Errorf("error get spell type %w", err)
+		}
+
+		// Spellテーブルへの挿入
+		_, err = spellRepo.InsertSpell(ctx, card.ID, spellType.ID)
+
+		if err != nil {
+			return fmt.Errorf("error create card %w", err)
+		}
+
+		result = card.ID
+		return nil
+	})
+
+	return result, err
+}
+
+func (n *neonUseCaseImpl) GetSpellCardByID(ctx context.Context, cardID int64) (carddto.SpellCardSelectResult, error) {
+	spellRepo := repository.NewSpellRepository(sqlc_gen.New(n.dbConn.DB))
+	return spellRepo.GetSpellByCardID(ctx, cardID)
+}
