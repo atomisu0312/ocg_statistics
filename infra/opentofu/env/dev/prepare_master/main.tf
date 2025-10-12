0d27e0e7f45fa3870ab75422d07f6eff2d2f6616
atomisu0312 <55617583+atomisu0312@users.noreply.github.com>
2025-10-12 20:29:32 +0900

    【カードマスタ】【インフラ】Airflowを既存のインフラ定義から分離 #134

--- infra/opentofu/env/dev/prepare_master/main.tf
+++ infra/opentofu/env/dev/prepare_master/main.tf
@@ -56,23 +56,3 @@ module "parameter_delta_id" {
   tag_base = var.tag_base
 }
 
-module "s3_for_mwaa" {
-  source = "../../../modules/s3/mwaa"
-  mwaa_bucket_name = var.mwaa_bucket_name
-  tag_base = var.tag_base
-}
-
-module "airflow" {
-  source = "../../../modules/airflow"
-
-  tag_base = var.tag_base
-  source_bucket_arn = module.s3_for_mwaa.mwaa_bucket_arn
-  airflow_version = "2.10.3"
-  account_id = local.account_id
-  region = var.region
-  internet_gateway_id = var.internet_gateway_id 
-  private_subnet_cidrs = var.private_subnet_cidrs
-  public_subnet_cidrs  = var.public_subnet_cidrs
-  vpc_id               = var.vpc_id
-  webserver_access_mode = var.webserver_access_mode
-}
