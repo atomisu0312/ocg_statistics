8dc00dbe40c4929dec83b3796aadb7e5567f56ba
atomisu0312 <55617583+atomisu0312@users.noreply.github.com>
2025-10-25 16:13:35 +0900

    【カードマスタ】【MWAA】ローカルで動くパイプラインを準備 #144

--- scripts/aws/preparemaster/get_ssm_parameters.sh
+++ scripts/aws/preparemaster/get_ssm_parameters.sh
@@ -18,6 +18,7 @@ echo "Retrieving SSM parameters..."
 # Construct full parameter names with a leading slash
 FULL_PARAM_NAME_DELTA="/${PARAMETER_NAME_DELTA_ID}"
 FULL_PARAM_NAME_CURRENT="/${PARAMETER_NAME_CURRENT_ID}"
+FULL_PARAM_NAME_MAX="/${PARAMETER_NAME_MAX_ID}"
 
 # --- Get deltaid ---
 DELTA_ID=$(aws ssm get-parameter --name "${FULL_PARAM_NAME_DELTA}" --query 'Parameter.Value' --output text)
@@ -33,6 +34,14 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+# --- Get maxid ---
+MAX_ID=$(aws ssm get-parameter --name "${FULL_PARAM_NAME_MAX}" --query 'Parameter.Value' --output text)
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to retrieve maxid parameter." >&2
+  exit 1
+fi
+
 echo "Successfully retrieved parameters:"
 echo "deltaid: $DELTA_ID"
 echo "currentid: $CURRENT_ID"
+echo "maxid: $MAX_ID"
