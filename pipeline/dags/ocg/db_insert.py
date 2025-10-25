9e59c6273c6c947f2c59ba2077a723a60c4f0b8c
atomisu0312 <koma4024@gmail.com>
2025-10-25 16:41:45 +0900

    【カードマスタ】【MWAA】【KAIZEN】decorator task と明示的 xcom_push が混在 #147

--- pipeline/dags/ocg/db_insert.py
+++ pipeline/dags/ocg/db_insert.py
@@ -12,49 +12,43 @@ from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeF
 from airflow.decorators import task, task_group
 
 @task(task_id="get_current_id_from_store")
-def _get_current_id_from_store(**context):
+def _get_current_id_from_store():
     """
     SSM Parameter Storeから値を取得する
     """
     ssm_path = os.environ.get('OCG_CURRENT_ID_SSM_PATH', 'invalid-ssm-path')
     ssm_hook = SsmHook(aws_conn_id='aws_default', region_name='ap-northeast-1')
     parameter_value = ssm_hook.get_parameter_value(parameter=ssm_path)
-    context["task_instance"].xcom_push(key="current_id", value=parameter_value)
     print(f"Got parameter value: {parameter_value}")
     return parameter_value
 
 @task(task_id="get_max_id_from_store")
-def _get_max_id_from_store(**context):
+def _get_max_id_from_store():
     """
     SSM Parameter Storeから値を取得する
     """
     ssm_path = os.environ.get('OCG_MAX_ID_SSM_PATH', 'invalid-ssm-path')
     ssm_hook = SsmHook(aws_conn_id='aws_default', region_name='ap-northeast-1')
     parameter_value = ssm_hook.get_parameter_value(parameter=ssm_path)
-    context["task_instance"].xcom_push(key="max_id", value=parameter_value)
     print(f"Got parameter value: {parameter_value}")
     return parameter_value
 
 @task(task_id="get_delta_id_from_store")
-def _get_delta_id_from_store(**context):
+def _get_delta_id_from_store():
     """
     SSM Parameter Storeから値を取得する
     """
     ssm_path = os.environ.get('OCG_DELTA_ID_SSM_PATH', 'invalid-ssm-path')
     ssm_hook = SsmHook(aws_conn_id='aws_default', region_name='ap-northeast-1')
     parameter_value = ssm_hook.get_parameter_value(parameter=ssm_path)
-    context["task_instance"].xcom_push(key="delta_id", value=parameter_value)
     print(f"Got parameter value: {parameter_value}")
     return parameter_value
 
 @task(task_id="update_current_id")
-def _update_current_id(**context):
+def _update_current_id(current_id_str: str, delta_id_str: str):
     """
     XComからcurrent_idを取得し、インクリメントしてSSM Parameter Storeに書き戻す
     """
-    current_id_str = context["task_instance"].xcom_pull(
-        task_ids="get_current_id_from_store", key="current_id"
-    )
     print(f"Current ID from XCom: {current_id_str}")
 
     if current_id_str is None:
@@ -63,9 +57,6 @@ def _update_current_id(**context):
 
     try:
         current_id = int(current_id_str)
-        delta_id_str = context["task_instance"].xcom_pull(
-            task_ids="get_delta_id_from_store", key="delta_id"
-        )
         print(f"Delta ID from XCom: {delta_id_str}")
         
         if delta_id_str is None:
@@ -93,29 +84,20 @@ def _update_current_id(**context):
         raise
 
 @task(task_id="alarm_excess_id")
-def _alarm_excess_id(**context):
+def _alarm_excess_id():
     print("Alarm excess id")
 
 @task.branch(task_id="choose_branch_path")
-def _choose_branch_path(**kwargs):
+def _choose_branch_path(current_id: str, delta_id: str, max_id: str):
     """
     This function determines which task to execute next based on a random choice.
     It returns the task_id of the chosen branch.
     """
     
-    current_id = kwargs["task_instance"].xcom_pull(
-        task_ids="get_current_id_from_store", key="current_id"
-    )
     print(f"Current ID from XCom: {current_id}")
     
-    delta_id = kwargs["task_instance"].xcom_pull(
-        task_ids="get_delta_id_from_store", key="delta_id"
-    )
     print(f"Delta ID from XCom: {delta_id}")
     
-    max_id = kwargs["task_instance"].xcom_pull(
-        task_ids="get_max_id_from_store", key="max_id"
-    )
     print(f"Max ID from XCom: {max_id}")
     
     if int(current_id) + int(delta_id) >= int(max_id):
@@ -131,8 +113,6 @@ with DAG(
     start = EmptyOperator(task_id="start")
 
     start_fetch_param = EmptyOperator(task_id="start_fetch_param")
-    
-    end_fetch_param = EmptyOperator(task_id="end_fetch_param")
 
     end = EmptyOperator(task_id="end")
 
@@ -141,7 +121,7 @@ with DAG(
     get_max_id_task = _get_max_id_from_store()
     get_delta_id_task = _get_delta_id_from_store()
     
-    choose_branch_path = _choose_branch_path()
+    choose_branch_path = _choose_branch_path(get_current_id_task, get_delta_id_task, get_max_id_task)
     alarm_excess_id_task = _alarm_excess_id()
     
     # 個別のタスクを定義
@@ -150,17 +130,17 @@ with DAG(
         region_name='ap-northeast-1',
         function_name=os.environ.get('OCG_LAMBDA_FUNCTION_NAME', 'invalid-lambda-function-name'),
         payload='''{
-            "startId": {{ task_instance.xcom_pull(task_ids='get_current_id_from_store', key='current_id') }},
-            "delta":{{ task_instance.xcom_pull(task_ids='get_delta_id_from_store', key='delta_id') }}
+            "startId": {{ task_instance.xcom_pull(task_ids='get_current_id_from_store') }},
+            "delta":{{ task_instance.xcom_pull(task_ids='get_delta_id_from_store') }}
         }''',
     )
     
-    update_id_task = _update_current_id()
+    update_id_task = _update_current_id(get_current_id_task, get_delta_id_task)
     
     end_update_task = EmptyOperator(task_id="end_update_task", trigger_rule="one_success")
 
-    start >> start_fetch_param >> [get_current_id_task, get_max_id_task, get_delta_id_task] >> end_fetch_param
-    end_fetch_param >> choose_branch_path >> [invoke_lambda_function_task, alarm_excess_id_task]
+    start >> start_fetch_param >> [get_current_id_task, get_max_id_task, get_delta_id_task]
+    choose_branch_path >> [invoke_lambda_function_task, alarm_excess_id_task]
     invoke_lambda_function_task >> update_id_task
     
     [update_id_task, alarm_excess_id_task] >> end_update_task
